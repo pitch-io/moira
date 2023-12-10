f6ce35b150a7221ee27df65d567acb10197c281e
Tibor Claassen <tc@codebeige.net>
2023-12-10 19:36:28 +0100

    Add documentation for `:app-log` integration

--- src/moira/log/txs.cljs
+++ src/moira/log/txs.cljs
@@ -1,4 +1,7 @@
 (ns moira.log.txs
+  "Interceptors for integrating `:app-log` into the
+  [[moira.transition|transition]] flow."
+
   (:require [moira.log.event-emitter :as event-emitter]
             [moira.log.module :as log.module]
             [moira.transition :as-alias transition]))
@@ -12,6 +15,13 @@
         (update app :app-log #(merge log.module/default %))))
 
 (def inject
+  "[[moira.context|Interceptor]] for ensuring `:app-log` exists and the
+  Application Log API is available to every module.
+
+  Merges `:app-log` with the [[moira.log.module/default|default
+  implementation]] while keeping existing values unchanged. Adds `:app-log` to
+  each other module's `:deps`."
+
   {:name ::inject
    :enter (fn [ctx]
             (update ctx ::transition/app inject*))})
@@ -23,6 +33,13 @@
   ctx)
 
 (def pause
+  "[[moira.context|Interceptor]] to pause Application Events from being
+  triggered during a [[moira.transition|transition]].
+
+  Buffers all incoming [[moira.event|events]] until the transition has finished
+  or was canceled due to an error. Will flush the buffer by triggering to
+  Application Log once the system has settled."
+
   {:name ::pause
    :enter (fn [ctx]
             (doto-event-emitter ctx event-emitter/pause))
