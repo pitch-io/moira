c687e3f9383b7e6c990094c3fd6fe3c870c27ba0
Tibor Claassen <tc@codebeige.net>
2023-12-12 18:14:01 +0100

    Rewrite introduction to interceptors

--- src/moira/context.cljs
+++ src/moira/context.cljs
@@ -1,29 +1,41 @@
 (ns moira.context
-  "Implementation of the interceptor pattern based on an asynchronous promise
-  chain.
+  "Implementation of the [interceptor design
+  pattern](https://lambdaisland.com/episodes/interceptors-concepts) based on
+  asynchronous `Promise` chains.
 
-  Interceptors map hook functions to the three execution stages `:enter`,
-  `:leave`, and `:error`. All hook functions are optional, will be passed
-  the current execution context, and need to return an updated context.
+  The context map expresses the current state of execution in plain data,
+  including error handling and support for asynchronous execution. `ctx` is
+  updated in sequence by taking interceptors from `::queue` and passing them
+  onto `::stack` while applying the `:enter` function of each interceptor. Once
+  the `::queue` is empty, interceptors are popped from `::stack`, which causes
+  each interceptor's `:leave` function to be called in reverse order. At any
+  point, when an exception is thrown, it is caught, all queued interceptors are
+  terminated, and `::error` is associated with the context. The stack will
+  continue to handle active interceptors by calling their respective `:error`
+  function for clean-up.
 
-  When executing a context, first each `:enter` hook is triggered in order.
-  After that, each intereptor's `:leave` hook will be invoked in reverse order.
-  At any point, when an error occurs the execution of remaining interceptors is
-  terminated and the `:error` function of previously processed interceptors is
-  triggered in reverse order.
+  An interceptor chain, called `txs`, is a sequential collection of
+  interceptors to be executed in order. Each `tx` connects to one or more
+  execution stages by mapping functions to `:enter`, `:leave`, and `:error`
+  respectively. Any of those functions will be called on the current execution
+  context and must return an updated `ctx` or a `Promise` resolving to the new
+  context. Note that interceptor functions are perfectly valid to adjust
+  `::queue` or `::stack` to alter execution. Similarly, returning a new context
+  with the `::error` key removed will settle an error. To signal an error,
+  return a context with the exception attached to `::error`.
 
-  The `::queue` of pending interceptors and `::stack` of partially processed
-  inteceptors are always present on the context. It is perfectly valid for
-  interceptor hooks to alter those in order to change execution.
+  Moira supports nesting of execution by operating on multiple qualified
+  `::queue`, `::stack`, and `::error` keys. To enable this, relevant functions
+  require a namespace prefix `n` as second argument. This feature enables
+  independent execution while providing access to a shared context. Setting up
+  a [[moira.transition|transition]] through interceptors is clearly separated
+  from executing `::txs` on each [[moira.module|module]] individually."
 
-  The current error is available via `::error`. To catch an error return a
-  context with the key removed. To signal an error return a context with the
-  `::error` key present."
   (:require [clojure.spec.alpha :as s]
             [promesa.core :as p]))
 
 (s/def ::enter fn?)
-(s/def ::error (partial instance? js/Error))
+(s/def ::error fn?) ; FIXME: (fn [ctx error] ,,,)
 (s/def ::leave fn?)
 (s/def ::tx (s/keys :opt-un [::enter ::error ::leave]))
 (s/def ::txs (s/coll-of ::tx))
